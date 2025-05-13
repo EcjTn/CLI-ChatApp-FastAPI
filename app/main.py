870d09f7fb2bfb5c62c3f9a00cbff740ce0854ef
EcjTn <ecjtaneo@gmail.com>
2025-05-13 11:43:10 +0800

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, Path, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+chatroom1 = {}
+chatroom2 = {}
+message_counter1 = 0  # Global counter for message IDs
+message_counter2 = 0  # Global counter for message IDs
+
+
+class MessageFormat(BaseModel):
+    username: str
+    message: str
+
+
+@app.post("/messages/room/1")
+async def send_msg(msg: MessageFormat):
+
+    global message_counter1
+    message_counter1 += 1
+
+    if msg.username == "":
+        msg.username = "Anonymous"
+
+
+
+    chatroom1[message_counter1] = {
+        "username": msg.username,
+        "message": msg.message
+    }
+    return "Welcome to Room 1", chatroom1
+
+
+
+
+@app.post("/messages/room/2")
+async def send_msg(msg: MessageFormat):
+
+    global message_counter2
+    message_counter2 += 1
+
+    if msg.username == "":
+        msg.username = "Anonymous"
+
+
+
+    chatroom2[message_counter2] = {
+        "username": msg.username,
+        "message": msg.message
+    }
+    return "Welcome to Room 2", chatroom2
+
+
+
+
+
+
+
+@app.get("/messages/room/")
+def showRooms():
+    return {
+        "Room 1": chatroom1,
+        "Room 2": chatroom2
+    }
---
+++ client/send_msg.py
@@ -0,0 +1,122 @@
+import requests
+import json
+import random
+import string
+import os
+
+
+letras = string.ascii_letters
+letras += string.digits
+
+
+
+
+
+
+usrPicked = input("Enter username: ")
+
+if usrPicked == "":
+    for x in range(18):
+        usrPicked += random.choice(letras)
+
+
+
+
+
+
+roomPicked = input('Enter Room number(1/2): ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    msgPicked = input("Your message: ")
+
+
+    url = f"http://127.0.0.1:8000/messages/room/{roomPicked}"
+
+
+    data = {
+     "username": usrPicked,
+        "message": msgPicked
+    }
+
+    os.system("cls")
+
+
+
+
+
+
+
+
+
+    response = requests.post(url, json=data)
+
+
+#print(json.dumps(response.json(), indent=5))
+
+#print("the json response")
+
+
+    response_data = response.json()
+
+
+
+    chatroom = response_data[1]
+
+
+
+
+
+
+
+    print("CHATROOM 2")
+    for msg_id in sorted(chatroom):
+        msg = chatroom[msg_id]
+        print(f"[{msg_id}] {msg['username']}: {msg['message']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
